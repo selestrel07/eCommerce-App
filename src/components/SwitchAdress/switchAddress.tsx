087e75e33a9f9a793ec1937cfa28a4113292becf
Татьяна Пулатова <tatanapulatova@MacBook-Pro-Tatana.local>
2025-05-19 10:20:18 +0500

    feat: add switch address component

---
+++ src/components/SwitchAdress/switchAddress.tsx
@@ -0,0 +1,41 @@
+import { Switch } from 'antd';
+import React, { useState } from 'react';
+import { SwitchAddressProps } from '../../interfaces/address/switchAdress';
+
+const SwitchAddress: React.FC<SwitchAddressProps> = ({ onChange }) => {
+  const [defaultAddresses, setDefaultAddresses] = useState({
+    defaultShipping: false,
+    defaultBilling: false,
+  });
+
+  const handleSwitchChange = (type: 'shipping' | 'billing', checked: boolean) => {
+    const newValues = {
+      ...defaultAddresses,
+      [`default${type.charAt(0).toUpperCase() + type.slice(1)}`]: checked,
+    };
+
+    setDefaultAddresses(newValues);
+    onChange(newValues);
+  };
+
+  return (
+    <div className="switch-group">
+      <div className="switch-item">
+        <span>Set as default address shipping</span>
+        <Switch
+          checked={defaultAddresses.defaultShipping}
+          onChange={(checked: boolean) => handleSwitchChange('shipping', checked)}
+        />
+      </div>
+      <div className="switch-item">
+        <span>Set as default address billing</span>
+        <Switch
+          checked={defaultAddresses.defaultBilling}
+          onChange={(checked: boolean) => handleSwitchChange('billing', checked)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SwitchAddress;
