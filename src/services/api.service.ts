5eca2d872a33f8e4c0486d5a3bdcfe2ad06248c2
selestrel07 <selestrel07@gmail.com>
2025-05-25 11:58:36 +0200

    feat: implement customer information load through commercetools api

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,6 +2,7 @@ import { apiRootBuilder } from './clientBuilder.ts';
 import { handleApiError } from './errorHandler.ts';
 import { Client } from '@commercetools/sdk-client-v2';
 import { mapAuthError } from './authService.ts';
+import { Customer } from '@commercetools/platform-sdk';
 
 export const loadProducts = async (client: Client) => {
   const apiRoot = apiRootBuilder(client);
@@ -15,6 +16,18 @@ export const loadProducts = async (client: Client) => {
     return httpResponse.body;
   } catch (rawError: unknown) {
     // Convert CommerceTools errors to readable ones
+    const humanReadableMsg = handleApiError(rawError);
+    throw mapAuthError(humanReadableMsg);
+  }
+};
+
+export const loadCustomerData = async (client: Client): Promise<Customer> => {
+  const apiRoot = apiRootBuilder(client);
+
+  try {
+    const httpResponse = await apiRoot.me().get().execute();
+    return httpResponse.body;
+  } catch (rawError: unknown) {
     const humanReadableMsg = handleApiError(rawError);
     throw mapAuthError(humanReadableMsg);
   }
