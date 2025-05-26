bce7c6ba2667a13eda4b0f07861870f1f4e0927d
selestrel07 <selestrel07@gmail.com>
2025-05-26 12:24:08 +0200

    feat: add enum with action types

---
+++ src/enums/edit-actions/edit-actions.ts
@@ -0,0 +1,9 @@
+export enum EditAction {
+  CHANGE_EMAIL = 'changeEmail',
+  SET_FIRST_NAME = 'setFirstName',
+  SET_LAST_NAME = 'setLastName',
+  ADD_ADDRESS = 'addAddress',
+  CHANGE_ADDRESS = 'changeAddress',
+  REMOVE_ADDRESS = 'removeAddress',
+  SET_DATE_OF_BIRTH = 'setDateOfBirth',
+}
