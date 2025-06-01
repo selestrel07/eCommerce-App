a8817ce619cfc290bf1a9f1c4e9ff2e74897dd05
selestrel07 <selestrel07@gmail.com>
2025-06-01 09:52:50 +0200

    fix: update address comparator function

--- src/utils/customer-field.utils.ts
+++ src/utils/customer-field.utils.ts
@@ -36,17 +36,17 @@ export const getCustomerFieldAddressIds = (
 export const addressComparator = (
   address1: Address,
   address2: Address,
-  defaultBillingAddress: string,
-  defaultShippingAddress: string
+  defaultShippingAddress: string,
+  defaultBillingAddress: string
 ): number => {
   if (defaultBillingAddress.length === 0 && defaultShippingAddress.length === 0) {
     return 0;
   } else if (address1.id === defaultShippingAddress) {
-    return 1;
-  } else if (address1.id === defaultBillingAddress) {
     return -1;
   } else if (address2.id === defaultShippingAddress) {
     return 1;
+  } else if (address1.id === defaultBillingAddress) {
+    return -1;
   } else {
     return 1;
   }
