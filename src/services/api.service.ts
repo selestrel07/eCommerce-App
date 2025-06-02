b9f19d2ee8e20a345f442c939172ea41c2ff05ad
selestrel07 <selestrel07@gmail.com>
2025-06-02 19:19:28 +0200

    feat: integrate category and breadcrumbs components into the Catalog page

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,11 +2,8 @@ import { apiRootBuilder } from './clientBuilder';
 import { handleApiError } from './errorHandler';
 import { Client } from '@commercetools/sdk-client-v2';
 import { mapAuthError } from './authService';
-import { ProductProjection } from '@commercetools/platform-sdk';
-import { ProductWithPrice } from '../interfaces/product/product';
-import { getProductPrice } from '../utils/productPrice';
-import { extractAttributes } from '../utils/extractAttributes';
-
+import { Customer, MyCustomerUpdateAction, ProductProjection } from '@commercetools/platform-sdk';
+import { QueryParams } from '../interfaces/query-params/query-params.ts';
 
 export const loadProducts = async (
   client: Client,
@@ -14,7 +11,7 @@ export const loadProducts = async (
   country?: string,
   customerGroupId?: string,
   channelId?: string
-): Promise<ProductWithPrice[]> => {
+): Promise<ProductProjection[]> => {
   const apiRoot = apiRootBuilder(client);
 
   try {
@@ -31,57 +28,7 @@ export const loadProducts = async (
       })
       .execute();
 
-    const products: ProductProjection[] = httpResponse.body.results;
-
-    return products.map((product) => {
-      const masterVariant = product.masterVariant;
-
-      const getVariantPrice = (variant: typeof masterVariant) =>
-        getProductPrice(
-          {
-            ...product,
-            masterVariant: variant,
-          },
-          currency
-        );
-
-      const masterPrice = getVariantPrice(masterVariant);
-      const image =
-        masterVariant?.images?.find((img) => img.label === 'Main')?.url ??
-        masterVariant?.images?.[0]?.url;
-
-      return {
-        id: product.id,
-        key: product.key!,
-        name: product.name,
-        description: product.description,
-        image,
-        price: masterPrice,
-        masterVariant: {
-          id: masterVariant.id,
-          key: masterVariant.key ?? undefined,
-          sku: masterVariant.sku ?? undefined,
-          price: masterPrice,
-          images: masterVariant.images ?? [],
-          attributes: extractAttributes(product, masterVariant),
-        },
-        variants: (product.variants || []).map((variant) => {
-          const variantPrice = getProductPrice({
-            ...product,
-            masterVariant: variant,
-          });
-
-          return {
-            id: variant.id,
-            key: variant.key ?? undefined,
-            sku: variant.sku ?? undefined,
-            price: variantPrice ?? undefined,
-            images: variant.images ?? [],
-            attributes: extractAttributes(product, variant),
-          };
-        }),
-      };
-    });
+    return httpResponse.body.results;
   } catch (rawError: unknown) {
     const humanReadableMsg = handleApiError(rawError);
     throw mapAuthError(humanReadableMsg);
@@ -148,5 +95,31 @@ export const changePassword = async (
   } catch (rawError: unknown) {
     const humanReadableMsg = handleApiError(rawError);
     throw new Error(humanReadableMsg);
+  }
+};
+
+export const loadCategories = async (client: Client) => {
+  const apiRoot = apiRootBuilder(client);
+  try {
+    return await apiRoot.categories().get().execute();
+  } catch (rawError: unknown) {
+    const humanReadableMsg = handleApiError(rawError);
+    throw new Error(humanReadableMsg);
+  }
+};
+
+export const searchProducts = async (client: Client, args: QueryParams) => {
+  const apiRoot = apiRootBuilder(client);
+  try {
+    const httpResponse = await apiRoot
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: args,
+      })
+      .execute();
+    return httpResponse.body.results;
+  } catch (rawError: unknown) {
+    throw new Error(handleApiError(rawError));
   }
 };
