4176e1a4a03d30c43803aee1326da4daa41d98f9
selestrel07 <selestrel07@gmail.com>
2025-06-06 14:26:49 +0200

    feat: add a link to the catalog page to the empty cart message

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,9 +1,14 @@
+import { Paths } from '@enums';
 import { FC, ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Cart: FC = (): ReactElement => {
   return (
     <>
-      <h2>There are no items in your cart. Please proceed with Catalog to add items.</h2>
+      <h2>
+        There are no items in your cart. Please proceed with <Link to={Paths.CATALOG}>Catalog</Link>{' '}
+        to add items.
+      </h2>
     </>
   );
 };
