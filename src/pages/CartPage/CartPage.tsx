f555ffe0f9bc9326deed50c57701ad6dcda15a0f
selestrel07 <selestrel07@gmail.com>
2025-06-06 10:58:27 +0200

    feat: implement a message for the empty cart

---
+++ src/pages/CartPage/CartPage.tsx
@@ -0,0 +1,10 @@
+import { Cart } from '@components';
+import './CartPage.scss';
+
+export default function CartPage() {
+  return (
+    <div className="cart-page-container">
+      <Cart />
+    </div>
+  );
+}
