b57b0616ef820a9ce9f664e52f41e7b9f5f3f435
heresyhawkins <heresyhawkins@gmail.com>
2025-06-11 15:01:41 +0300

    feat: complete about us page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,6 +3,8 @@ import { Row, Col, Card, Typography } from 'antd';
 import DimaImg from '../../assets/aboutUs/member_1.jpg';
 import TanyaImg from '../../assets/aboutUs/member_2.jpg';
 import EgorImg from '../../assets/aboutUs/member_3.jpg';
+import LogoSchool from '../../assets/aboutUs/logo_school.png';
+import LogoGit from '../../assets/aboutUs/git_logo.png';
 import './About.scss';
 
 const { Title } = Typography;
@@ -11,9 +13,12 @@ const GRID_GUTTER = 32;
 export const AboutUsPage: React.FC = () => {
   return (
     <div>
-      <Title level={2} style={{ textAlign: 'center', marginBottom: 40 }}>
+      <Title className="about-title" level={2}>
         About Us
       </Title>
+      <a href="https://rs.school/">
+        <img className="logo-school" src={LogoSchool} alt="" />
+      </a>
 
       <Row gutter={[GRID_GUTTER, GRID_GUTTER]}>
         <Col xs={24} md={8}>
@@ -25,6 +30,9 @@ export const AboutUsPage: React.FC = () => {
               description="About me: graduated from BSU with a degree in Mathematics (teacher of mathematics and computer science), 2 years worked in school as a teacher of mathematics and computer science, then in a bank as a tester and system analyst, then moved to IT and worked as QA Automation in two companies. Hobbies: I like to learn something new (more often IT related), read, play video games, go to soccer with my wife.
 "
             />
+            <a href="https://github.com/selestrel07">
+              <img className="logo-git" src={LogoGit} alt="" />
+            </a>
           </Card>
         </Col>
 
@@ -41,6 +49,9 @@ During my maternity leave I got interested in digital art and then in frontend d
 Among my hobbies are reading books, constantly striving for self-development, video games, long walks and cooking: I love to cook and delight my loved ones with my dishes.
 "
             />
+            <a href="https://github.com/isvaya">
+              <img className="logo-git" src={LogoGit} alt="" />
+            </a>
           </Card>
         </Col>
 
@@ -48,6 +59,9 @@ Among my hobbies are reading books, constantly striving for self-development, vi
           <Card cover={<img alt="member3" src={EgorImg} />}>
             <Card.Meta title="Egor Zhitkovski" />
             <Card.Meta title="Developer" description="sdasd" />
+            <a href="https://github.com/heresyhawkins">
+              <img className="logo-git" src={LogoGit} alt="" />
+            </a>
           </Card>
         </Col>
       </Row>
