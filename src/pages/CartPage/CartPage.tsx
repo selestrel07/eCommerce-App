f65e8c294fa64d30ac830c686da7e2d2495887f5
heresyhawkins <heresyhawkins@gmail.com>
2025-06-16 16:18:41 +0300

    feat: complete recomendation

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,30 +1,11 @@
-import { use, useEffect } from 'react';
 import { Client } from '@commercetools/sdk-client-v2';
-import { loadCart } from '@services';
-import { CartContext } from '@contexts';
 import './CartPage.scss';
 import { Cart } from '@components';
 
 export default function CartPage({ client }: { client: Client }) {
-  const { setCart, setCartItemsCount } = use(CartContext);
-
-  const refreshCart = async () => {
-    try {
-      const response = await loadCart(client);
-      setCart(response);
-      setCartItemsCount(response.lineItems.length);
-    } catch (error) {
-      console.error('Failed to refresh cart', error);
-    }
-  };
-
-  useEffect(() => {
-    void refreshCart();
-  }, [client]);
-
   return (
     <div className="cart-page-container">
-      <Cart onCartUpdate={() => void refreshCart()} client={client} />
+      <Cart onCartUpdate={undefined} client={client} />
     </div>
   );
 }
